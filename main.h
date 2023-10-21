cc0534b5582a35002ddf6c739d9d59d75e41b21f
oralcj <nnochirioral@gmail.com>
2023-10-21 07:46:56 -0700

    peer group

---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef main_H
+#define main_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
